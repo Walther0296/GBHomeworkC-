c01eebfa4fac187de89c2b78ec579ae517fe99dd
Walther0296 <sau100@inbox.ru>
2023-07-26 14:05:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №1. 
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Hello, World!");
+
+// Задача №2
+Console.WriteLine("Hello, World!");
+
+// Задача №3
+Console.WriteLine("Hello, World!");
+
+// Задача №4
+Console.WriteLine("Hello, World!");
